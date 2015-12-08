3be01025cdc5a3a2754220a185f6b5129bd3adf8
George Uwah <georgeuwah@rhs.org.uk>
2015-12-08 10:39:21 +0000

    08-12-15Commit

--- Venue-Management-Mvn-Proj/src/test/java/crm/cucumberFramework/Venue_Management_Mvn_Proj/DateTimePicker.java
+++ Venue-Management-Mvn-Proj/src/test/java/crm/cucumberFramework/Venue_Management_Mvn_Proj/DateTimePicker.java
@@ -36,7 +36,7 @@ public class DateTimePicker {
         driver.get("https://www.google.com/");
         
        // driver.get("http://vmcrmuat:9010/#/cfsg/courseadmin/courses/3");
-        // System.out.println("this fir testing purpose only Peace Uwah ")
+        // System.out.println("this fir testing purpose only ")
  
          
  
